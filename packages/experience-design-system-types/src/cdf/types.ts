cb74ca6ff9a8ce4a48dfdc3b7c3877e38aaac373
Tyler Pina <tyler.pina@contentful.com>
2026-08-25 08:43:57 -0600

    feat: add $token.sets and $token.allowed to the CDF schema (#167)

--- packages/experience-design-system-types/src/cdf/types.ts
+++ packages/experience-design-system-types/src/cdf/types.ts
@@ -43,8 +43,14 @@ export interface CDFValidationError {
   actual?: string;
 }
 
+export interface CDFValidationWarning {
+  path: string;
+  message: string;
+}
+
 export interface CDFValidationResult {
   valid: boolean;
   errors: CDFValidationError[];
+  warnings: CDFValidationWarning[];
   components: Array<{ key: string; entry: CDFComponentEntry }>;
 }
