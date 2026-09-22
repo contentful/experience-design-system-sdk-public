38d665c30b55ac811688ab9d23b8cc0414f99da1
michaelpineirocontentful <michael.pineiro@contentful.com>
2026-09-22 08:44:49 -0600

    refactor(testing): remove PTY harness

--- tools/dsi-pty-harness/fixtures/projects/react-composite-cycle/src/Heading.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactElement } from 'react';
-
-export interface HeadingProps {
-  text: string;
-}
-
-export function Heading({ text }: HeadingProps): ReactElement {
-  return <h1>{text}</h1>;
-}
--- tools/dsi-pty-harness/fixtures/projects/react-invalid/src/components/DuplicateB.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export interface DuplicateProps2 {
-  title: string;
-}
-
-export function Duplicate(props: DuplicateProps2): React.ReactElement {
-  return <div>{props.title}</div>;
-}
